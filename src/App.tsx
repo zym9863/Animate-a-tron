f53b2445ed4007f474c6f996b14b8fe615a16673
zym <202213093023@cuc.edu.cn>
2025-04-06 18:12:07 +0800

    fix an issue

--- src/App.tsx
+++ src/App.tsx
@@ -75,8 +75,8 @@ function App() {
           ref={setPreviewElement}
           className={`preview-box ${isAnimating ? 'animating' : ''}`}
         >
-          <img src="src/assets/animation-concept-1.png" alt="Animation Concept 1" className="preview-image" />
-          <img src="src/assets/motion-graphics-2.png" alt="Motion Graphics 2" className="preview-image" />
+          <img src="/animation-concept-1.png" alt="Animation Concept 1" className="preview-image" />
+          <img src="/motion-graphics-2.png" alt="Motion Graphics 2" className="preview-image" />
         </div>
       </div>
 
